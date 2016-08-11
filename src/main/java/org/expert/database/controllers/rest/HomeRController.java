3b98b84ace6ec1b8be0708b129ad980296361b8b
Channy <sokchanny.sg@gmail.com>
2016-08-11 19:08:37 +0700

    lasrt

--- src/main/java/org/expert/database/controllers/rest/HomeRController.java
+++ src/main/java/org/expert/database/controllers/rest/HomeRController.java
@@ -43,8 +43,8 @@ public class HomeRController {
     @RequestMapping(value = {"/expert/find-by-skill-id/{skill_id}"}, method = RequestMethod.GET)
     public ResponseEntity<Map<String, Object>> findExpertBySkillId(@PathVariable("skill_id") int skill_id) {
         HttpEntity<Object> request = new HttpEntity<Object>(header);
-        ResponseEntity<Map> response = rest.exchange(
-                "http://192.168.178.137:8081/api/expert/find-by/skill-id/{skillId}?paramSkillId=2",
+        ResponseEntity<Map> response = rest.exchange(WS_URL + 
+                "/api/expert/find-by/skill-id/{skillId}?paramSkillId=2",
                 HttpMethod.GET, request, Map.class);
         return new ResponseEntity<Map<String, Object>>(response.getBody(), HttpStatus.OK);
     }
@@ -53,15 +53,13 @@ public class HomeRController {
     public ResponseEntity<Map<String, Object>> findExpertBySkill() {
         HttpEntity<Object> request = new HttpEntity<Object>(header);
         header.set("Accept", MediaType.APPLICATION_JSON_VALUE);
-        String URL = "http://192.168.178.137:8081/api/expert/find-by/skill-id/{skillId}?paramSkillId=2";
+        String URL = WS_URL + "/expert/find-by/skill-id/{skillId}?paramSkillId=2";
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(URL);
         builder.queryParam("paramSkillId", 2);
         ResponseEntity<Map> response = rest.exchange(
                 builder.build()
                 .encode()
-                .toUri(), HttpMethod.GET
-                , request
-                , Map.class);
+                .toUri(), HttpMethod.GET, request, Map.class);
         return new ResponseEntity<Map<String, Object>>(response.getBody(), HttpStatus.OK);
     }
 }
